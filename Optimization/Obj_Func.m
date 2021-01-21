be6c3daadbe66050af0fdfbfe26f865860182c0a
Ayush Sharan <ayush00414@gmail.com>
2021-01-22 00:48:54 +0530

    Added Optimizer and Objective function

---
+++ Optimization/Obj_Func.m
@@ -0,0 +1,39 @@
+function value = Obj_Func(x)
+        
+    global Aircraft
+    
+    Aircraft = Performance(Aircraft);
+    
+    Aircraft.Performance.WbyP = x(1);
+    Aircraft.Wing.Sweep_qc = x(2);
+    Aircraft.Wing.t_c_root = x(3);
+    Aircraft.Performance.M_cruise = x(4);
+    Aircraft.Performance.altitude_cruise1 = x(5);
+    Aircraft.Wing.taper_ratio = x(6);
+    Aircraft.Wing.Aspect_Ratio = x(7);
+%     Aircraft.Performance.WbyS = x(2);
+    Aircraft.Wing.S = x(8);
+    
+    Aircraft.Weight.MTOW = 15000;  % Initial Guess
+    error = 1; % Dummy value to start the while loop
+    
+    while error > 0.005
+    
+        error = Aircraft.Weight.MTOW;
+        Aircraft = Sizing(Aircraft);
+        Aircraft.Performance.WbyS = Aircraft.Weight.MTOW/Aircraft.Wing.S;
+        Aircraft = Aero(Aircraft);
+
+        Aircraft = Crew_Payload_Weight(Aircraft);
+        Aircraft = Empty_Weight(Aircraft);
+        Aircraft = Fuel_Weight(Aircraft);
+        Aircraft.Weight.MTOW = Aircraft.Weight.crew + Aircraft.Weight.payload + Aircraft.Weight.fuel_Weight...
+                               + Aircraft.Weight.empty_Weight;
+
+        error = abs(error - Aircraft.Weight.MTOW);
+    
+    end
+    
+    value = Aircraft.Weight.MTOW;
+   
+end
---
+++ Optimization/Optimizer.m
@@ -0,0 +1,34 @@
+% Code for finding optimum values of T/W, and W/S which gives minimum weight
+% with all constraints satisfied.
+clear
+clc
+close all
+
+global Aircraft
+Aircraft = struct();
+
+d2r = pi/180;
+
+LB = [0.2,25,0.11,0.7,34000,0.2,7,3000];  % Lower Bound
+UB = [0.6,35,0.15,0.85,40000,0.3,10,4100]; % Upper Bound
+
+
+A = [];
+B = [];
+Aeq = [];
+Beq = [];
+
+options = optimoptions('fmincon','Algorithm','sqp','Display','iter-detailed',...
+    'FunctionTolerance',1e-6,'OptimalityTolerance',1e-6,'ConstraintTolerance',1e-6,....
+    'StepTolerance',1e-20,'MaxFunctionEvaluations',600);
+ 
+%[X,~,exitflag,output] = ga(@(x) Obj_Func(x), x0, A, B, Aeq, Beq, LB, UB, @(x) Nonlincon(x),options);
+
+gs = GlobalSearch;
+
+problem = createOptimProblem('fmincon','x0',[0,0],...
+    'objective',sixmin,'lb',LB,'ub',UB,'nonlcon',@(x) Nonlincon(x),'options',options);
+[X,output] = run(gs,problem)
+                
+hold off;
+
